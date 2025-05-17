3a6860087720f18b03306a062f511c80403559a9
CyberAdam-ui <abashford.90@gmail.com>
2025-05-17 11:52:25 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "statebucket29" {
+  bucket = "statebucket29"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "statebucket29"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
